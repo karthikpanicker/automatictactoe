5bde002ace28d5829596175298b77ad9e0ab6cbd
carmal <carmal.s@attinadsoftware.com>
2019-09-04 21:47:19 +0530

    added 4 env as part of travis build env

--- etsello-deploy.sh
+++ etsello-deploy.sh
@@ -3,7 +3,10 @@
 
 DOCKER_IMAGE=$1
 BUILD_DIR=$2
-TRELLO_CONSUMER_SECRET=$3
+ETSY_CONSUMER_KEY=$3
+ETSY_CONSUMER_SECRET=$4
+TRELLO_CONSUMER_KEY=$5
+TRELLO_CONSUMER_SECRET=$6
 executor()
 {
 cd $BUILD_DIR
@@ -12,8 +15,6 @@ docker-compose down
 docker rmi -f $(docker images | grep $DOCKER_IMAGE | tr -s ' ' | cut -d ' ' -f 3)
 docker images
 docker-compose pull --quiet
-TRELLO_CONSUMER_SECRET=$TRELLO_CONSUMER_SECRET docker-compose up -d --force-recreate
+TRELLO_CONSUMER_SECRET=$TRELLO_CONSUMER_SECRET ETSY_CONSUMER_KEY=$ETSY_CONSUMER_KEY ETSY_CONSUMER_SECRET=$ETSY_CONSUMER_SECRET TRELLO_CONSUMER_KEY=$TRELLO_CONSUMER_KEY docker-compose up -d --force-recreate
 }
-
 executor
-
